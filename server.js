c18de37c6fac287a99aae4cdaa82fbabd9a9e5c1
zaid bhimala <zaidm124@gmail.com>
2022-02-13 11:24:47 +0530

    heroku

--- server.js
+++ server.js
@@ -43,9 +43,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(express.static(path.resolve(__dirname, "public")));
 
-var temp = "";
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.get("/trans", async (req, res) => {
   // res.sendFile(__dirname + "/index.html");
